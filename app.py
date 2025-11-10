7ef2dfa9606af94525b4423096f9808805f65b88
Mahmoud Dahlan <mahmoudmmsd10@gmail.com>
2025-11-09 20:45:43 -0500

    chore: add app, tests, CI, CodeQL, Dependabot, Bandit config

---
+++ app.py
@@ -0,0 +1,7 @@
+
+def insecure_function(password: str):
+    # Intentionally unsafe for practice purposes
+    print("Your password is:", password)
+
+if __name__ == "__main__":
+    insecure_function("admin123")
